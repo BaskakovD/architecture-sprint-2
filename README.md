cd76275cbc2da786fcc6653567ca6a99316840d8
Баскаков Дмитрий Евгеньевич <baskakov-d@datagile.ru>
2025-03-19 12:02:47 +0300

    task1-task6

---
+++ README.md
@@ -0,0 +1,59 @@
+# Задание 2. Шардирование
+
+Директория mongo-sharding
+
+`cd mongo-sharding`
+
+## Запуск проекта
+
+1. Запускаем `docker compose`
+
+   `docker compose up -d`
+
+2. Инициируем базу данных
+
+   `./mongo-sharding.sh`
+
+3. Проверяем количество документов
+
+   `./mongo-db-checker.sh`
+
+Графическое решение см. в файле task1-task6.drawio лист Task2
+
+# Задание 3. Репликация
+
+Директория mong-sharding-repl
+
+## Запуск проекта
+
+1. Запускаем docker-compose
+
+`docker compose up -d`
+
+2. Инициируем базу данных
+
+   `./mongo-sharding-repl.sh`
+
+Графическое решение см. в файле task1-task6.drawio лист Task3
+
+# Задание 4. Кэширование
+
+## Запуск проекта
+
+1. Запускаем docker-compose
+
+`docker compose up -d`
+
+2. Инициируем базу данных
+
+`./mongo-sharding-repl.sh`
+
+Графическое решение см. в файле task1-task6.drawio лист Task4
+
+# Задание 5. Service Discovery и балансировка с API Gateway
+
+Графическое решение см. в файле task1-task6.drawio лист Task5
+
+# Задание 5. CDN
+
+Графическое решение см. в файле task1-task6.drawio лист Task6
---
+++ sharding-repl-cache/scripts/sharding-repl-cache.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+###
+# Инициализируем сервер конфигураций
+###
+
+docker compose exec -T configSrv mongosh --port 27017 --quiet <<EOF
+rs.initiate(
+  {
+    _id : "config_server",
+       configsvr: true,
+    members: [
+      { _id : 0, host : "configSrv:27017" }
+    ]
+  }
+);
+EOF
+
+###
+# Инициализируем shard11
+###
+
+docker compose exec -T shard11 mongosh --port 27018 <<EOF
+rs.initiate(
+    {
+      _id : "shard1",
+      members: [
+        { _id : 0, host : "shard11:27018" },
+        { _id : 1, host : "shard12:27021" },
+        { _id : 2, host : "shard13:27022" }
+      ]
+    }
+);
+EOF
+
+###
+# Инициализируем shard21
+###
+
+docker compose exec -T shard21 mongosh --port 27019 <<EOF
+rs.initiate(
+    {
+      _id : "shard2",
+      members: [
+        { _id : 0, host : "shard21:27019" },
+        { _id : 1, host : "shard22:27023" },
+        { _id : 2, host : "shard23:27024" }
+      ]
+    }
+);
+EOF
+
+###
+# Инициализируем роутер
+###
+
+docker compose exec -T mongos_router mongosh --port 27020 <<EOF
+sh.addShard( "shard1/shard11:27018,shard12:27021,shard13:27022");
+sh.addShard( "shard2/shard21:27019,shard22:27023,shard23:27024");
+
+sh.enableSharding("somedb");
+sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } );
+
+use somedb
+for(var i = 0; i < 1000; i++) db.helloDoc.insert({age:i, name:"ly"+i});
+
+db.helloDoc.countDocuments();
+EOF
